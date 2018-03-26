d7c498afd16c3a27213ecc42d613c7d7278be737
Chris Casciano <ccasciano@automatastudios.com>
2018-03-26 17:07:47 -0400

    add mini-css-extract-plugin for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-// const path = require('path');
+const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  // entry: './src/index.js',
-  // output:  {
-  //   filename: 'bundle.js',
-  //   path: path.resolve(__dirname, 'dist')
-  // }
-
+let config = {
+  entry: './src/index.js',
+  output:  {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
   module: {
     rules: [
       {
@@ -23,26 +23,9 @@ module.exports = {
         ]
       },
       {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'postcss-loader'
-          },
-          {
-            loader: 'sass-loader'
-          },
-        ]
-      },
-      {
         test: /\.html$/,
         use: {
-          loader: "html-loader",
+          loader: 'html-loader',
           options: { minimize: true }
         }
       }
@@ -50,8 +33,54 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+      chunkFilename: '[name].css',
+    }),
   ]
 };
+
+// TODO: investigate how to do this with the mode argument
+// https://github.com/webpack/webpack/issues/6460
+if (JSON.stringify(process.env.NODE_ENV) === `"production"`) {
+  config.module.rules.push({
+    test: /\.scss$/,
+    use: [
+      {
+        loader: MiniCssExtractPlugin.loader
+      },
+      {
+        loader: 'css-loader'
+      },
+      {
+        loader: 'postcss-loader'
+      },
+      {
+        loader: 'sass-loader'
+      },
+    ]
+  });
+} else {
+  config.module.rules.push({
+    test: /\.scss$/,
+    use: [
+      {
+        loader: 'style-loader'
+      },
+      {
+        loader: 'css-loader'
+      },
+      {
+        loader: 'postcss-loader'
+      },
+      {
+        loader: 'sass-loader'
+      },
+    ]
+  });
+}
+
+module.exports = config;
